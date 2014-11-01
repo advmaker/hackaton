521e810b1be60ada8574081063e57f3deeaded04
Anton V. Nazarov <a.nazarov@advmaker.net>
2014-11-01 17:14:04 +0300

    magic!

--- server.go
+++ server.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-//	"github.com/go-martini/martini"
-//	"github.com/martini-contrib/render"
-//  "github.com/martini-contrib/auth"
-	"github.com/eaigner/hood"
+	_ "github.com/lib/pq"
+	"database/sql"
+	//	"github.com/go-martini/martini"
+	//	"github.com/martini-contrib/render"
+	//  "github.com/martini-contrib/auth"
+	//	"github.com/eaigner/hood"
 	"log"
+	"fmt"
 )
 
 func main() {
-
-	db, err := hood.Open("postgres", "host=127.0.0.1 port=5432 dbname=achievements_db user=root password=аа sslmode=verify-full")
+	//	db, err := sql.Open("postgres", "postgres://root:G4rr6Df7l@hotfile25.com")
+	db, err := sql.Open("postgres", "host=hotfile25.com port=5432 dbname=achievements_db user=root password=G4rr6Df7l")
+	//	db, err := sql.Open("postgres", "host=localhost port=5432 dbname=achievements_db user=vagrant password=vagrant")
 	if err != nil {
-//		log.Fatal(err)
+		log.Fatal(err)
 	} else {
-		some := db.QueryRow("SELECT (1+1) AS some")
-		log.Fatal(some)
+		var s string
+		db.QueryRow("SELECT 1+1 as c").Scan(&s)
+		fmt.Println(s)
 	}
 }
