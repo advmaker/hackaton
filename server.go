112604af0401e86d939af0e071fbe83ebb50ebdf
Maksim (Ellrion) Platonov <ellrion11@gmail.com>
2015-01-10 12:37:51 +0300

    test structure

--- server.go
+++ server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/advmaker/hackaton/achievement"
+
 )
 
 func main() {
-	app := achievement.Application{}
-	app.Run()
+//	app := achievement.Application{}
+//	app.Run()
 }
