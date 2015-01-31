16f86abaefa3acc0fb366e8887f62320b390440d
Maksim Denisov <m.denisov@advmaker.net>
2015-01-31 16:50:36 +0300

    Templates

--- app/controllers/users_controller.go
+++ app/controllers/users_controller.go
@@ -3,11 +3,15 @@ package controllers
 import (
 	"github.com/jinzhu/gorm"
 	"log"
+
+	"github.com/martini-contrib/render"
 )
 
 type UsersController struct{ Controller }
 
-func (controller *UsersController) New(db gorm.DB, l *log.Logger) string {
-	l.Print("123")
-	return "some"
+func (controller *UsersController) New(db gorm.DB, l *log.Logger, r render.Render) {
+
+
+	r.HTML(200,"hello","oggggg")
+
 }
